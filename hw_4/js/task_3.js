9cc5cc38966c22c69b17c44bf9bccc0404c951c6
Yaroslav <64233157+Yaroslav-ciber@users.noreply.github.com>
2020-08-05 00:50:12 -0700

    hw_4

---
+++ hw_4/js/task_3.js
@@ -0,0 +1,10 @@
+// for (let i = 0; i < 18; i += 1) {
+//     console.log(i)
+// }
+
+const repeatLog = function (n) {
+  for (let i = 0; i < n; i += 1) {
+    console.log(i);
+  }
+};
+repeatLog(9);
